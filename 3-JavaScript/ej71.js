cac068d94559204690ca2ba616d04c5834311fe1
OscarVelandia <odvs12@gmail.com>
2017-10-27 22:56:07 -0300

    Modificación de ej's 71 y 81 para hacer el código más performante

--- 3-JavaScript/ej71.js
+++ 3-JavaScript/ej71.js
@@ -1,15 +1,16 @@
 let numero = 9;
 
-while (numero < 90){
-    console.log(numero);
-    numero += 9;  
-}
+// while (numero < 90){
+//     console.log(numero);
+//     numero += 9;  
+// }
 
 //Otra versión usando un multiplicador
-let multiplicador = 1;
+let multiplicador = 1; 
 let resultado = null;
+let multiplicarHasta = 15; //Agrego esta variable para que sea más dinámico el programa
 
-while (resultado < 90){
+while (multiplicador <= multiplicarHasta){
     resultado = numero * multiplicador;
     console.log(resultado);
     multiplicador++;      
--- 3-JavaScript/ej81.js
+++ 3-JavaScript/ej81.js
@@ -1,6 +1,17 @@
-let numero = 9;
+let tabla = 9;
+
+// do {
+//     console.log(tabla);
+//     tabla += 9;  
+// } while (tabla < 90);
+
+//Otra versión usando un multiplicador
+let multiplicador = 1; 
+let resultado = null;
+let multiplicarHasta = 12; //Agrego esta variable para que sea más dinámico el programa
 
 do {
-    console.log(numero);
-    numero += 9;  
-} while(numero <= 90)
+    resultado = tabla * multiplicador;
+    console.log(resultado);
+    multiplicador++;      
+} while (multiplicador <= multiplicarHasta);
