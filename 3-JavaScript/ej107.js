dc81cf1b8e608872ca2780629685aee338812add
OscarVelandia <odvs12@gmail.com>
2017-11-02 08:15:20 -0300

    Uso de arrow function y cambio en el algoritmo.

--- 3-JavaScript/ej107.js
+++ 3-JavaScript/ej107.js
@@ -1,10 +1,13 @@
-function numeroMasGrande(numero1,numero2) {
-    if (typeof numero1, typeof numero2 != 'number'){
-        console.log('Esta función espera valores del tipo number');
-    } else if (numero1 >= numero2) {
-       console.log(`El número ${numero1} es más grande que ${numero2}`);
-    } else if ( numero1 == numero2){
-        return console.log('Los dos números son ${numero1}');
+let numeroMasGrande = (numero1, numero2) => {
+    switch (true) { 
+    //Usé true para que directamente entre a evaluar los parametros de la función.
+        case (typeof numero1, typeof numero2 != 'number'):
+            console.log('Esta función espera valores del tipo number');
+        case (numero1 >= numero2):
+            console.log(`El número ${numero1} es más grande que ${numero2}`);
+            break;
+        case (numero1 == numero2):
+            console.log('Los dos números son ${numero1}');
     }
 }
 
