d12e33fb50890e755d1a642d14a26bf0a72974a6
OscarVelandia <odvs12@gmail.com>
2017-11-02 22:54:33 -0300

    uso de interpolacion de textos para imprimir elementos en el array.

--- 3-JavaScript/ej117.js
+++ 3-JavaScript/ej117.js
@@ -1,5 +1,3 @@
 let alumnos = ['Melisa', 'Carolina', 'Eze', 'Diego', 'criss'];
 
-console.log(alumnos[0]);
-console.log(alumnos[2]);
-console.log(alumnos[4]);
+console.log(`${alumnos[0]} ${alumnos[2]} ${alumnos[4]}`);
