2fbbebd614c4a0eeb822329c98e9799d6030fd65
OscarVelandia <odvs12@gmail.com>
2017-11-02 22:46:30 -0300

    cambio de algoritmo.

--- 3-JavaScript/ej116.js
+++ 3-JavaScript/ej116.js
@@ -1,41 +1,41 @@
 const mejorJugadorArgentino = (numero) => {
     switch (numero) {
         case 18:
-            console.log(`${numero}, Nery Pumpido`);
+            return `${numero}, Nery Pumpido`;
             break;
         case 5:
-            console.log(`${numero}, José Luis Brown`);
+            return `${numero}, José Luis Brown`;
             break;
         case 9:
-            console.log(`${numero}, José Cuciuffo`); 
+            return `${numero}, José Cuciuffo`; 
             break;
         case 19:
-            console.log(`${numero}, Óscar Ruggeri`);
+            return `${numero}, Óscar Ruggeri`;
             break;
         case 14:
-            console.log(`${numero}, Ricardo Giusti`);
+            return `${numero}, Ricardo Giusti`;
             break;
         case 16: 
-            console.log(`${numero}, Julio Olarticoechea`); 
+            return `${numero}, Julio Olarticoechea`; 
             break;
         case 2:
-            console.log(`${numero}, Sergio Batista`); 
+            return `${numero}, Sergio Batista`; 
             break;
         case 12:
-            console.log(`${numero}, Héctor Enrique`);
+            return `${numero}, Héctor Enrique`;
             break;
         case 7: 
-            console.log(`${numero}, Jorge Burruchaga`);
+            return `${numero}, Jorge Burruchaga`;
             break;
         case 10:
-            console.log(`${numero}, Diego Armando Maradona`); 
+            return `${numero}, Diego Armando Maradona`; 
             break;
         case 11:
-            console.log(`${numero}, Jorge Valdano`);
+            return `${numero}, Jorge Valdano`;
             break;
         default:
-            console.log('Carlos Bilardo');
+            return 'Carlos Bilardo';
     }
 }
 
-mejorJugadorArgentino(36);
+console.log(mejorJugadorArgentino(11));
