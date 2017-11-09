553cc49d26016fb4ad03f9b654988ed924d3efff
OscarVelandia <odvs12@gmail.com>
2017-11-09 14:05:06 -0300

    Comentario agregado.

--- 3-JavaScript/ej133.js
+++ 3-JavaScript/ej133.js
@@ -1,7 +1,7 @@
 const mutantes = ['Prof. Charles Francis Xavier', 'Scott Summers', 'Dr. Henry Philip "Hank" McCoy', 'Jean Elaine Grey', 'Calvin Montgomery Rankin', 'Kevin Sydney', 'Lorna Sally Dane', 'Alexander Summers', 'Suzanne Chan', 'James "Logan" Howlett', 'Ororo Monroe'];
 
 if (mutantes.indexOf('Lorna Sally Dane') != -1){
-   
+    //Pide agregar break pero en el if no es posible.
 } else {
     console.log(mutantes.toString());
 };
